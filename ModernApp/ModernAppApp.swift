8abb1e70516a05f396d1c06b70d2755a2c2684a1
n salehi <nimasalehi.auth@icloud.com>
2025-11-08 19:19:15 +0330

    Initial Commit

---
+++ ModernApp/ModernAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernAppApp.swift
+//  ModernApp
+//
+//  Created by Nima Salehi on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ModernAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
