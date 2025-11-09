3159a21a8c64be37d53b6ef460023f55970f643f
n salehi <nimasalehi.auth@icloud.com>
2025-11-09 14:13:08 +0330

    init

--- ModernApp/ContentView.swift
+++ ModernApp/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import ProfileKit
 struct ContentView: View {
     var body: some View {
         VStack {
--- ModernApp/ModernAppApp.swift
+++ ModernApp/ModernAppApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import ProfileKit
 @main
 struct ModernAppApp: App {
     var body: some Scene {
